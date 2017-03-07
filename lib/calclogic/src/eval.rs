6fd0b3b8a780a4919dd32df848e715e1ebb714c7
Quinten Palmer <quinten@clypd.com>
2017-03-06 23:04:12 -0500

    evaling

--- lib/calclogic/src/eval.rs
+++ lib/calclogic/src/eval.rs
@@ -1,5 +1,15 @@
-use models::{Result, Value};
+use models::{Result, Error, Value, Operation};
 
 pub fn eval(v: Value) -> Result<Value> {
-        return Ok(Value::Num(8));
+    return match v {
+        Value::Num(n) => Ok(Value::Num(n)),
+        Value::Operate(op, v1, v2) => {
+            let eval1 = try!(eval(*v1));
+            let eval2 = try!(eval(*v2));
+            match (op, eval1, eval2) {
+                (Operation::Plus, Value::Num(n1), Value::Num(n2)) => Ok(Value::Num(n1 + n2)),
+                _ => Err(Error::EvalNonNum),
+            }
+        }
+    };
 }
